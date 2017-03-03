9a63870b32bd394d67e5c826c4638ef9fdd1025d
lapinoub <ant.le.berre@gmail.com>
2017-03-03 01:50:50 +0100

    part7

---
+++ SiteShareprod/test/controller/admin/afficher_user.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 03/03/2017
+ * Time: 01:35
+ */
+
---
+++ SiteShareprod/test/controller/admin/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 02/03/2017
+ * Time: 23:40
+ */
+
+include_once ("app/view/layout/header.html");
