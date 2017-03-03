16e4fddb551547dd74261797692854834d0cf748
lapinoub <ant.le.berre@gmail.com>
2017-03-03 01:36:12 +0100

    part5

--- SiteShareprod/index.php
+++ SiteShareprod/index.php
@@ -1,20 +1,20 @@
 <?php
 
 include_once ("app/model/pdo.inc.php");
-include_once ("core/core_view/paginet.php");
-include_once ("core/core_view/helper.php");
-include_once ("core/core_controlleur/protection.php");
-include_once ("core/core_controlleur/location.php");
+//include_once ("core/core_view/paginet.php");
+//include_once ("core/core_view/helper.php");
+//include_once ("core/core_controlleur/protection.php");
+//include_once ("core/core_controlleur/location.php");
 include_once ("core/core_model/count.php");
 include_once ("core/core_model/lire_table.php");
 include_once ("core/core_model/delete_rows.php");
 include_once ("core/core_model/insert_row.php");
 include_once("app/config/config.inc.php");
-include_once ("librairies/secu_sessionStart.php");
+//include_once ("librairies/secu_sessionStart.php");
 
-if (!mySessionStart()) {
-	die("FAIL");
-}
+//if (!mySessionStart()) {
+//	die("FAIL");
+//}
 
 if (!isset($_GET["module"])) 
 {
