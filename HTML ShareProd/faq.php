f835b3b6128561e7abccc41fd87de9eae857516c
Korhonen <Korhonen@users.noreply.github.com>
2017-03-18 15:38:54 +0100

    Add files via upload

--- HTML ShareProd/faq.php
+++ HTML ShareProd/faq.php
@@ -4,15 +4,35 @@
 	<div class="content">
 	<h2>FAQ</h2>
 	Vous trouverez ici les questions les plus couramment posés.
-	<div class="question-faq">1/ Lorem ipsum dolor sit amet, consectetur adipisicing elit ?</div>
-	<div class="reponses-faq">Sit dolore, incidunt id nihil velit nam est a cumque totam, dolores voluptas ea. Nobis perspiciatis, provident nisi magni deserunt reiciendis ratione.</div>
-	<div class="question-faq">2/ Lorem ipsum dolor sit amet, consectetur adipisicing elit ?</div>
-	<div class="reponses-faq">Sit dolore, incidunt id nihil velit nam est a cumque totam, dolores voluptas ea. Nobis perspiciatis, provident nisi magni deserunt reiciendis ratione.</div>
-	<div class="question-faq">3/ Lorem ipsum dolor sit amet, consectetur adipisicing elit ?</div>
-	<div class="reponses-faq">Sit dolore, incidunt id nihil velit nam est a cumque totam, dolores voluptas ea. Nobis perspiciatis, provident nisi magni deserunt reiciendis ratione.</div>
-	<div class="question-faq">4/ Lorem ipsum dolor sit amet, consectetur adipisicing elit ?</div>
-	<div class="reponses-faq">Sit dolore, incidunt id nihil velit nam est a cumque totam, dolores voluptas ea. Nobis perspiciatis, provident nisi magni deserunt reiciendis ratione.</div>
-
+	<div class="question-faq">1/ Qu’est-ce que ShareProd ?</div>
+	<div class="reponses-faq">ShareProd est une plateforme de financement participatif dédiée 100% aux Webséries. Nos services vous permettent d’organiser une collecte de fonds afin de financer un projet et de le diffuser.</div>
+	<div class="question-faq">2/ Quels types de projets sont acceptés ?</div>
+	<div class="reponses-faq">Longs-métrages, courtmétrages, documentaires, web-documentaires, films d’animations, jeux vidéos, web-série, projet d’émission pilote, etc. Pour être retenus, les projets devront être conformes à la loi évitant notamment les propos à caractère raciste, pornographique, violent, injurieux, diffamatoire, etc.</div>
+	<div class="question-faq">3/ Qu’est-ce que le	financement participatif ?</div>
+	<div class="reponses-faq">Le financement participatif (ou crowdfunding) s’est développé dans les années 2000 notamment grâce à l’essor des réseaux sociaux. Ce nouveau type d’économie collaborative consiste à mobiliser une communauté autour d’un projet. <br/><br/>
+	ShareProd vous permet ainsi d’impliquer votre réseau (du plus proche au plus lointain) dans la recherche de financement et la diffusion de votre film. <br/><br/>
+	L’avantage est que cela permet de réaliser des projets qui n’auraient pas pu voir le jour par les circuits traditionnels, de les tester auprès du public, de les adapter et de créer une communauté tout en participant à une vraie aventure humaine.</div>
+	<div class="question-faq">4/ Comment fonctionne une collecte de fond sur ShareProd ?</div>
+	<div class="reponses-faq">Chaque projet est déposé et géré de manière indépendante par le porteur de projet. Il assume et contrôle complètement la responsabilité artistique.<br/><br/>
+	Celui-ci choisit une durée, un montant et remplit la page de dépôt de projet. Une fois complète, un coach ShareProd intervient et le guide.<br/><br/>
+	ShareProd est une plateforme de financement participatif avec récompenses financières et non-financières (parfois appelée « dons avec contreparties »).<br/><br/>
+	Dès qu’un internaute investit sur un projet, il devient « coprod » et bénéficie de récompenses. Dès la campagne lancée, le porteur doit communiquer et faire  connaitre son projet afin de générer du trafic sur sa page et d’attirer des coprods potentiels. À la fin, la campagne est dite « financée » si l’objectif est atteint. À l’inverse, elle sera considérée comme « échouée », ShareProd fonctionnant selon la règle du « tout ou rien ».</div>
+	<div class="question-faq">5/ Les producteurs ont-ils des droits	 sur les œuvres ?</div>
+	<div class="reponses-faq">Non, le coprod ne bénéficie d’aucun droit intellectuel sur le film. Celui-ci peut bénéficier uniquement de récompenses non financières et d’un visionnage du film.<br/><br/>
+	Le porteur conserve l'entière propriété intellectuelle et les droits de son projet.</div>
+	<div class="question-faq">6/ Qui peut lancer	un projet sur ShareProd ?</div>
+	<div class="reponses-faq">La plateforme est ouverte à tous ceux qui souhaitent réaliser un projet audiovisuel sans aucune restriction de nationalité, de format ou de genre. Le porteur de projet doit être une personne physique ou morale.<br/><br/>
+	Le porteur est responsable des droits de son projet et garantit qu’il ne reproduit pas sans autorisation une œuvre protégée par la propriété intellectuelle.</div>
+	<div class="question-faq">7/ Que permet l’inscription ?</div>
+	<div class="reponses-faq">L’inscription permet de :<br/><br/>
+	- Déposer un projet sur ShareProd<br/><br/>
+	- Produire un projet et le suivre<br/><br/>
+	- Poster des commentaires sur les pages projets<br/><br/>
+	- Recevoir la newsletter ShareProd</div>
+	<div class="question-faq">8/ Comment se rémunère ShareProd ?</div>
+	<div class="reponses-faq">Si le projet atteint 100% de sa jauge, il est dit « financé ». ShareProd prélève alors une commission de 5% du montant récolté + 3% de frais de transactions sécurisées (soit 8% TTC).<br/><br/>
+	Si le projet n’atteint pas son objectif, aucune commission n’est prélevée et les
+	coprods sont remboursés.</div>
 	<div class="others-faq">D'autres questions ?</div>
 	<a href=""><div class="btn-faq btn">Contactez-nous</div></a>
 	</div>
