f7e6a797f19f90fff6cf18862ba71ad350732821
lapinoub <ant.le.berre@gmail.com>
2017-03-18 17:20:04 +0100

    part39

--- shareprod/app/view/projets/projets.php
+++ shareprod/app/view/projets/projets.php
@@ -1,61 +1,131 @@
 <?php include_once ("app/view/layout/header.php") ?>
 
- <div class="bgwhite">
- 	<h3>Tous les projets</h3>
-  	<div class="cat">
- 		FILTRER PAR :
+<div class="content">
+    <h3>Tous les projets</h3>
+    <div class="cat">
+        FILTRER PAR :
         <?php foreach ($tabCategories as $tabCategory) { ?>
-            <a href="?module=projets&action=projets&cat=<?= $tabCategory["id_cat"] ?>"><div class="cat1">
-                    <img src="webroot/icone_cat/<?= $tabCategory["icone_cat"]?>.bmp" alt="">
-                    <?= $tabCategory["nom_cat"]; ?>
-                </div></a>
+        <a href="?module=projets&action=projets&cat=<?= $tabCategory["id_cat"] ?>"><div class="cat1">
+                <img src="img/icons/financement-goal.svg" alt=""/>
+                <?= $tabCategory["nom_cat"]; ?>
+            </div></a>
         <?php } ?>
- 	</div>
- 	<div class="bandebleu"></div>
- 	<div class="liste-projets">
-         <?php while ($i < $affichage) {
-             ?>
-            <a href="?module=projets&action=detail&id=<?= $tabProjets[$i]["id_projets"] ?>"><div class="projet1">
-                 <img src="webroot/image_miniature/<?= $tabProjets[$i]["image_miniature"] ?>.bmp" alt="">
-                 <div class="txt-projet">
-                     <h3><?= $tabProjets[$i]["titre_projets"] ?></h3>
-                     <p><?= $tabProjets[$i]["texte_miniature"] ?></p>
-                     <div class="barre-chargement"></div>
-                     <div class="argent-restant">
-                         <div class="img-argent-restant"></div>
-                         <?= $tabProjets[$i]["avancement_projets"] ?>€ / <?= $tabProjets[$i]["financement_max_projets"] ?>€ restants
-                     </div>
-                     <div class="temps-restant">
-                         <div class="img-temps-restant"></div>
-                         <?= $tabProjets[$i][5] ?> jours restants
-                     </div>
-                 </div>
-                </div></a>
-         <?php  $i++;  } ?>
     </div>
-     <div class="liste-projets">
-         <?php while ($i < sizeof($tabProjets)) {
-             ?>
-             <a href="?module=projets&action=detail&id=<?= $tabProjets[$i]["id_projets"] ?>"><div class="projet1">
-                 <img src="webroot/image_miniature/<?= $tabProjets[$i]["image_miniature"] ?>.bmp" alt="">
-                 <div class="txt-projet">
-                     <h3><?= $tabProjets[$i]["titre_projets"] ?></h3>
-                     <p><?= $tabProjets[$i]["texte_miniature"] ?></p>
-                     <div class="barre-chargement"></div>
-                     <div class="argent-restant">
-                         <div class="img-argent-restant"></div>
-                         <?= $tabProjets[$i]["avancement_projets"] ?>€ / <?= $tabProjets[$i]["financement_max_projets"] ?>€ restants
-                     </div>
-                     <div class="temps-restant">
-                         <div class="img-temps-restant"></div>
-                         <?= $tabProjets[$i][5] ?> jours restants
-                     </div>
-                 </div>
-             </div></a>
-             <?php  $i++;  } ?>
-     </div>
-     <div class="bandebleu"></div>
-	<div class="btn-plusprojet btn"><?php paginat($nbProjets, NB_PROJETS_PAGE, "projets", "projets", $option);?></div>
-	</div>
+    <div class="bandebleu"></div>
+    <div class="liste-projets">
+        <div class="projet1">
+            <img src="img/bg/success1.png" alt="">
+            <div class="txt-projet">
+                <div class="nom-projet">Nom du projet</div>
+                <p>See owner, run in terror mrow, unwrap toilet paper, yet missing until dinner time. If it fits, i sits chase the pig around the house.</p>
+                <div class="barre-chargement">
+                    <div class="barre-chargement-avancement"></div>
+                </div>
+                <div class="argent-restant">
+                    <div class="img-argent-restant"></div>
+                    100000€ / 120000€ restants
+                </div>
+                <div class="temps-restant">
+                    <div class="img-temps-restant"></div>
+                    30 jours restants
+                </div>
+            </div>
+        </div>
+        <div class="projet1">
+            <img src="img/bg/success1.png" alt="">
+            <div class="txt-projet">
+                <div class="nom-projet">Nom du projet</div>
+                <p>See owner, run in terror mrow, unwrap toilet paper, yet missing until dinner time. If it fits, i sits chase the pig around the house.</p>
+                <div class="barre-chargement">
+                    <div class="barre-chargement-avancement"></div>
+                </div>
+                <div class="argent-restant">
+                    <div class="img-argent-restant"></div>
+                    100000€ / 120000€ restants
+                </div>
+                <div class="temps-restant">
+                    <div class="img-temps-restant"></div>
+                    30 jours restants
+                </div>
+            </div>
+        </div>
+        <div class="projet1">
+            <img src="img/bg/success1.png" alt="">
+            <div class="txt-projet">
+                <div class="nom-projet">Nom du projet</div>
+                <p>See owner, run in terror mrow, unwrap toilet paper, yet missing until dinner time. If it fits, i sits chase the pig around the house.</p>
+                <div class="barre-chargement">
+                    <div class="barre-chargement-avancement"></div>
+                </div>
+                <div class="argent-restant">
+                    <div class="img-argent-restant"></div>
+                    100000€ / 120000€ restants
+                </div>
+                <div class="temps-restant">
+                    <div class="img-temps-restant"></div>
+                    30 jours restants
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bandebleu"></div>
+    <div class="liste-projets">
+        <div class="projet1">
+            <img src="img/bg/success1.png" alt="">
+            <div class="txt-projet">
+                <div class="nom-projet">Nom du projet</div>
+                <p>See owner, run in terror mrow, unwrap toilet paper, yet missing until dinner time. If it fits, i sits chase the pig around the house.</p>
+                <div class="barre-chargement">
+                    <div class="barre-chargement-avancement"></div>
+                </div>
+                <div class="argent-restant">
+                    <div class="img-argent-restant"></div>
+                    100000€ / 120000€ restants
+                </div>
+                <div class="temps-restant">
+                    <div class="img-temps-restant"></div>
+                    30 jours restants
+                </div>
+            </div>
+        </div>
+        <div class="projet1">
+            <img src="img/bg/success1.png" alt="">
+            <div class="txt-projet">
+                <div class="nom-projet">Nom du projet</div>
+                <p>See owner, run in terror mrow, unwrap toilet paper, yet missing until dinner time. If it fits, i sits chase the pig around the house.</p>
+                <div class="barre-chargement">
+                    <div class="barre-chargement-avancement"></div>
+                </div>
+                <div class="argent-restant">
+                    <div class="img-argent-restant"></div>
+                    100000€ / 120000€ restants
+                </div>
+                <div class="temps-restant">
+                    <div class="img-temps-restant"></div>
+                    30 jours restants
+                </div>
+            </div>
+        </div>
+        <div class="projet1">
+            <img src="img/bg/success1.png" alt="">
+            <div class="txt-projet">
+                <div class="nom-projet">Nom du projet</div>
+                <p>See owner, run in terror mrow, unwrap toilet paper, yet missing until dinner time. If it fits, i sits chase the pig around the house.</p>
+                <div class="barre-chargement">
+                    <div class="barre-chargement-avancement"></div>
+                </div>
+                <div class="argent-restant">
+                    <div class="img-argent-restant"></div>
+                    100000€ / 120000€ restants
+                </div>
+                <div class="temps-restant">
+                    <div class="img-temps-restant"></div>
+                    30 jours restants
+                </div>
+            </div>
+        </div>
+    </div>
+    <a href=""><div class="btn-plusprojet btn">Voir plus de projets</div></a>
+</div>
 
 <?php include_once ("app/view/layout/footer.php") ?>
